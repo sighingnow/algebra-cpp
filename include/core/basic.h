92e02dca7a50bb98a17b7b486ecfcb9ab4c094d4
He Tao <hetao@buaa.edu.cn>
2016-01-04 21:34:56 +0800

    Prepare dev environment.

---
+++ include/core/basic.h
@@ -0,0 +1,14 @@
+/**
+ * The MIT License(MIT). Copyright (c) 2015-2016 He Tao
+ */
+
+#ifndef __LIBFTL_CORE_BASIC_H__
+#define __LIBFTL_CORE_BASIC_H__
+
+namespace ftl {
+
+};
+
+#endif /* __LIBFTL_CORE_BASIC_H__ */
+
+
---
+++ test/helloworld_test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include <autocheck/autocheck.hpp>
+#include <bandit/bandit.h>
+#include "ftl_reporter.h"
+
+template <typename A, typename B = A>
+bool add(A const & a, A const & b) {
+    return a + b == b + a;
+}
+
+go_bandit([](){
+    ftl_reporter reporter;
+    bandit::describe("Hello world:", [&](){
+        bandit::before_each([&](){
+
+        });
+
+        bandit::it("add correct", [&](){
+            autocheck::check<int, int>(add<int, int>, 100, autocheck::make_arbitrary<int, int>(), reporter);
+            AssertThat(reporter.correct(), IsTrue());
+        });
+    });
+});
+
+int main(int argc, char* argv[])
+{
+  return bandit::run(argc, argv);
+}
