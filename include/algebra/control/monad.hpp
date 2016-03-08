275d3149f0c293f3c07d714eb471fc3683a643aa
He Tao <hetao@buaa.edu.cn>
2016-03-08 20:47:43 +0800

    Rename .hh to .hpp.

--- include/algebra/control/monad.hpp
+++ include/algebra/control/monad.hpp
@@ -2,10 +2,10 @@
  * The MIT License(MIT). Copyright (c) 2015-2016 He Tao
  */
 
-#ifndef __ALGEBRA_CONTROL_MONAD_HH__
-#define __ALGEBRA_CONTROL_MONAD_HH__
+#ifndef __ALGEBRA_CONTROL_MONAD_HPP__
+#define __ALGEBRA_CONTROL_MONAD_HPP__
 
-#include "../basic/type_operation.hh"
+#include "../basic/type_operation.hpp"
 
 /**
  * Monad: represents sequenceable computations in a context.
@@ -55,8 +55,42 @@ namespace algebra {
             return value;
         }
     };
+
+    /**
+     * Default definition for `pure`, `bind`, `map` and `join`.
+     */
+
+    template<typename M>
+    struct default_pure {
+        using T = ValueType<M>;
+        static constexpr M pure(const T& t)
+                noexcept(std::is_nothrow_constructible<M, const T&>::value) {
+            return M{ t };
+        }
+        static constexpr M pure(T&& t)
+                noexcept(std::is_nothrow_constructible<M, T&&>::value) {
+            return M{ std::move(t) };
+        }
+    };
+
+    template<typename _M>
+    struct default_bind {
+        using T = ValueType<_M>;
+
+        template<typename U>
+        using M = Rebind<_M, U>;
+
+        template<typename F, typename U = ValueType<ResultOf<F(T)>>>
+        static M<U> bind(const M<T>& m, F&& f) {
+            return monad<M<U>>::join(monad<M<T>>::map(std::forward<F>(f), m));
+        }
+        template<typename F, typename U = ValueType<ResultOf<F(T)>>>
+        static M<U> bind(M<T>&& m, F&& f) {
+            return monad<M<U>>::join(monad<M<T>>::map(std::forward<F>(f), std::move(m)));
+        }
+    };
 }
 
-#endif /* __ALGEBRA_CONTROL_MONAD_HH__ */
+#endif /* __ALGEBRA_CONTROL_MONAD_HPP__ */
 
 
